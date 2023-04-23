2cbb46641574347b95dcedfeb674b70a06a2a54c
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-04-23 13:06:22 -0400

    Stage 2

--- Phone Book (Kotlin)/task/src/phonebook/Main.kt
+++ Phone Book (Kotlin)/task/src/phonebook/Main.kt
@@ -1,42 +1,40 @@
 package phonebook
 
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 
 fun main() {
-    val initTime = System.currentTimeMillis()
+    val currentDir = System.getProperty("user.dir")
+    val contactsFile = File("$currentDir/directory.txt")
+    val namesFile = File("$currentDir/find.txt")
 
     println("Start searching...")
 
-    val currentDir = System.getProperty("user.dir")
-    val file = File("$currentDir/directory.txt")
+    val elapsedTimeMillis = measureTimeMillis {
+        val lines = readContactsFromFile(contactsFile)
+        val names = namesFile.readLines()
+        val linearSearchResults = linearSearch(names, lines)
 
-    val lines: List<Contact> = file.readLines().map {
-        val (number, name) = it.split(" ", limit = 2)
-        Contact(number, name)
+        println("Found ${linearSearchResults.size} / ${names.size} entries.")
     }
 
-    val names = File("$currentDir/find.txt").readLines()
-    val linearSearch = linearSearch(names, lines)
-    val elapsedTimeMillis = System.currentTimeMillis() - initTime
     val seconds = elapsedTimeMillis / 1000
     val minutes = seconds / 60
     val millis = elapsedTimeMillis % 1000
 
-    println("Found ${linearSearch.size} / ${names.size} entries. Time taken: $minutes min. $seconds sec. $millis ms.")
+    println("Time taken: $minutes min. $seconds sec. $millis ms.")
 }
 
-fun linearSearch(lines: List<String>, contacts: List<Contact>): List<String> {
-    val found = mutableListOf<String>()
-
-    lines.forEach { name ->
-        for (contact in contacts) {
-            if (contact.name == name) {
-                found.add(contact.toString())
-                break
-            }
-        }
+fun readContactsFromFile(file: File): List<Contact> {
+    return file.readLines().map {
+        val (number, name) = it.split(" ", limit = 2)
+        Contact(number, name)
     }
+}
 
-    return found
+fun linearSearch(names: List<String>, contacts: List<Contact>): List<Contact> {
+    return names.mapNotNull { name ->
+        contacts.firstOrNull { contact -> contact.name == name }
+    }
 }
